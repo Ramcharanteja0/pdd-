11dead006b3b54da73be92466f1b3af30ebed50a
Ramcharanteja0 <prasanthprasad123456@gmail.com>
2026-05-23 19:48:51 +0530

    fix: correct typo in supabase URL

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://hpdnuczygunnkndzklaik.supabase.co';
+const supabaseUrl = 'https://hpdnuczygunkndzklaik.supabase.co';
 const supabaseKey = 'sb_publishable_Wlvq-QD7k68Vi_LNEi9mWA_UiarPpgX';
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
